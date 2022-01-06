8519cd08f3b2ba0903a1a53e5d8ac79ef5321e53
Daniel Charlak <danielcharlak@wp.pl>
2022-01-06 23:20:45 +0100

    main file
    Scenario with 0 on x postion added. Still cleaning.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include "array.h"
 using namespace std;
 
 
@@ -11,30 +12,22 @@ private :
     int cols;
     float ** array;
 
-
-    void print2DArray(float** arr, int rows, int cols);
-    void print2DArray(float ar[][5],int rows, int cols );
-    void printEquationFromMatrix();
-    float** copy2DArrayToPointer(float arr[][5],int rows, int cols);
-    void getPrepared2DArray();
     void pickInput();
     void pickMethod();
-    bool isDiagonalValueEqualZero(int i, int j);
+    void getPrepared2DArray();
+    void setArray(float ** ar, int r, int c);
+
 
     float* arrayWithCoefficients(int step);
     void defaultElimination();
     float* getResultFromRowEchelonForm();  // macierz schodkowa
+    bool isDiagonalValueEqualZero(int i, int j);
+    bool isFirstElementEqualZero();
 public :
 
-
-
+    ~Gauss(){delete2Darray(this->array, this->rows);}
     void menu();
-    ~Gauss(){
-        for(int i = 0; i < this->rows; ++i) {
-            delete [] this->array[i];
-        }
-        delete [] this->array;
-    }
+
 };
 
 
@@ -54,8 +47,8 @@ int main(){
 }
 
 void Gauss::menu(){
-    this->pickInput();
-    this->pickMethod();
+    pickInput();
+    pickMethod();
 
 }
 
@@ -81,88 +74,64 @@ void Gauss::pickInput(){
 void Gauss::getPrepared2DArray(){
 
     float example1[4][5] = { {1,1,0,-3,1}, {1,4,-1,-4,-2},{0.5,0.5,-3,-5.5,1.5} ,{1.5,3,-5,-9,-0.5}};
-    float example2[4][5] = {{1,2,-1,2,0} ,{1,0,-2,4,4}, {0,-3,1.5,7,0}, {0,-1,1,6,-1}};
-    float example3[4][5] = { {9,10,7,-21,-33}, {-3,-7.5,6.5,0,17},{-6.25,-12.5,0.25,5.25,24.25},{2.25,-2.5,4,-5.25,-1}};
-
+    float example2[4][5] = {{0,2,-1,2,0} ,{0,0,-2,4,4}, {0,-3,1.5,7,0}, {1,-1,1,6,-1}};
+    float example3[4][5] = { {2.25,-2.5,4,-5.25,-1}, {-3,-7.5,6.5,0,17},{-6.25,-12.5,0.25,5.25,24.25},{9,10,7,-21,-33}};
+    float example4[3][4] = { {1,2,-1,-1},{-1,-1,3,4},{-2,-3,1,-1}};
 
     int chooice;
-    cout << " Przyklad 1 : wynik poprawny  [-1,-1,1,-1]";
-    this->print2DArray(example1, 4, 5);
-    cout << " Przyklad 2 : wynik poprawny  [0,-1,-2,0]";
-    this->print2DArray(example2, 4, 5);
-    cout << " Przyklad 3 : wynik poprawny  x4=1 x3=1 x2=-1 x1=-1";
-    this->print2DArray(example3, 4, 5);
+    cout << "\nPrzyklad 1 : wynik poprawny  x4=-1 x3=1 x2=-1 x1=-1";
+    print2DArray(example1, 4, 5);
+    cout << "\nPrzyklad 2 : wynik poprawny  x4=0 x3=-2 x2=-1 x1=0";
+    print2DArray(example2, 4, 5);
+    cout << "\nPrzyklad 3 : wynik poprawny  x4=1 x3=1 x2=-1 x1=-1";
+    print2DArray(example3, 4, 5);
+    cout << "\nPrzyklad 4 : wynik poprawny   x3=2 x2=-1 x1=3";
+    float ** tempP = copy2DArrayToPointer(example4, 3,4);
+    print2DArray(tempP, 3, 4);
+    delete2Darray(tempP, 3);
     cout <<"\n wybierz przyklad : ";
     cin >> chooice;
     if(chooice == 1) {
-
-        this->rows = 4;
-        this->cols = 5;
         float **ptr = copy2DArrayToPointer(example1,4,5);
-        this->array = ptr;
+        this->setArray(ptr, 4,5);
     }
     else if(chooice == 2){
-        this->rows = 4;
-        this->cols = 5;
         float **ptr = copy2DArrayToPointer(example2,4,5);
-        this->array = ptr;
-
+        this->setArray(ptr, 4,5);
     }
     else if(chooice == 3){
-        this->rows = 4;
-        this->cols = 5;
         float **ptr = copy2DArrayToPointer(example3,4,5);
-        this->array = ptr;
-
+        this->setArray(ptr, 4,5);
+    }
+    else if(chooice == 4){
+        float **ptr = copy2DArrayToPointer(example4,3,4);
+        this->setArray(ptr, 3,4);
     }
 }
 
 
-float** Gauss::copy2DArrayToPointer(float arr[][5],int rows, int cols){
-    float **ptr = new float*[rows];
-    for (int i = 0; i < rows; ++i) {
-        ptr[i] = new float[cols];
-    }
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            ptr[i][j] = arr[i][j];
 
-        }
-    }
-    return ptr;
-}
+void Gauss::pickMethod(){
 
-void Gauss::print2DArray(float arr[][5],int rows, int cols ){
-  cout << endl;
-
-    for(int i = 0;i < rows ;i++){
-        for(int j = 0; j < cols; j++){
-            cout.width(5);
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-
-}
-
-
-void Gauss::print2DArray( float **arr,  int rows,int cols){
+    int choice = 0;
+    cout << " \n1.Podstawowa elimacja Gaussa\n2.Elminacja z wyborem elementu max" ;
     cout << endl;
-
-    for(int i = 0;i < rows ;i++){
-        for(int j = 0; j < cols; j++){
-            cout.width(5);
-            cout << arr[i][j] << " ";
+    cin >> choice;
+    switch(choice){
+        case 1:{
+            this->defaultElimination();
+            this->getResultFromRowEchelonForm();
+            break;
         }
-        cout << endl;
+        case 2:{
+
+            break;
+        }
+        default : {
+            cout << " wybrano zly numer ";
+            break;
+        }
     }
-
-}
-
-void Gauss::printEquationFromMatrix(){
-
-
 }
 float* Gauss::getResultFromRowEchelonForm(){
 
@@ -173,7 +142,6 @@ float* Gauss::getResultFromRowEchelonForm(){
     float temp = 0;
     rows--;
     result[rows] = (ar[rows][lastColumn])/(ar[rows][rows]);
-    print2DArray(ar,4,5);
     cout << "x"<<rows+1<< "=" << result[rows]<< "\t" ;
     for(int i = rows -1 ;i >= 0 ; i--){ // start od przedostatniego wiersza
         temp = 0 ;
@@ -204,77 +172,57 @@ float* Gauss::arrayWithCoefficients(int step){
     cout << "\n tablica wspolczynnikow\n ";
     for(int i = startingRow , j = step - 1; i < rows; i++){
         coef[i] = (this->array[i][j])/(this->array[j][j]);
-
         cout << coef[i] << " ";
-
         startingRow++;
     }
     return coef;
 }
-void Gauss::pickMethod(){
 
-    int choice = 0;
-    cout << " \n1.Podstawowa elimacja Gaussa\n2.Elminacja z wyborem elementu max\n0.wyjscie" ;
-    do {
-        cout << endl;
-        cin >> choice;
-        switch(choice){
-            case 1:{
-                this->defaultElimination();
-                this->getResultFromRowEchelonForm();
-                cout << "wynik = ";
-                break;
-            }
-
-            case 2:{
-
-                break;
-            }
-
-            case 0:{
-                cout << "wychodzenie " ;
-                break;
-            }
-
-            default : {
-                cout << " wybrano zly numer ";
-                break;
-            }
-        }
-    }while(choice != 0);
-
-}
 
 void Gauss::defaultElimination(){
     int step = 1;
     int rows = this->rows;
     int cols = this->cols;
-
-//TODO sprwadzicz czy 1 element nie jest rowny zero i jak tak to zamienic wiersze
-    float *coefficients  = arrayWithCoefficients(step);
-    while(step < rows){
-        cout << "\n step " << step << endl;
-        for(int i = step ; i < rows ; i++){
-            for(int j = step-1; j < cols ;j++){
-
-                if(this->isDiagonalValueEqualZero(i,j)){ // wartosc na przekatnej = 0
-                    //przesuniecie wierszy ?
-
-                }
-                else{ // wartosc na przekatnej != 0
-
-                   this->array[i][j] -=   (coefficients[i]*this->array[step-1][j]);
-                }
-
-            }
-
-            print2DArray(this->array,rows,cols);
+    int temp = 1;
+    while(isFirstElementEqualZero()){
+        if(temp == rows){
+            cout << "ukladu nie mozna rozwiazac tym sposobem";
+            exit(15);
         }
-        step++;
-        coefficients = arrayWithCoefficients(step);
+        swapRows(this->array, cols, 0, temp);
+        print2DArray(this->array,rows,cols);
+        temp++;
     }
 
+    float *coefficients  ;
+    while(step < rows){
+        coefficients = arrayWithCoefficients(step);
+        cout << "\n step " << step << endl;
+        if(isDiagonalValueEqualZero(i,j)){
+                    //przesuniecie wierszy ?
+            cout << " \n\n trzeba przesunac wiersze";
+            }
+        for(int i = step ; i < rows ; i++){
+            for(int j = step-1; j < cols ;j++){
+                this->array[i][j] -=   (coefficients[i]*this->array[step-1][j]);
+            }
+
+
+        }
+        print2DArray(this->array,rows,cols);
+        step++;
+
+    }
+
+}
+bool Gauss::isFirstElementEqualZero(){
+    return (this->array[0][0] <= this->numberCloseToZero);
 }
 bool Gauss::isDiagonalValueEqualZero(int i, int j){
     return i == j && fabs(this->array[i][j]) <= this->numberCloseToZero;
 }
+void Gauss::setArray(float ** ar, int r, int c){
+        this->array = ar;
+        this->rows = r;
+        this->cols = c;
+    }
