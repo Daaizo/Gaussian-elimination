fdad9ae7a506c2da05f95be9f9ac931c850a3311
Daniel Charlak <danielcharlak@wp.pl>
2022-01-07 11:23:06 +0100

    main file
    swapping rows if one of the diagonal values is 0, added some error messages when there is no solutions or there is infinitely many

--- main.cpp
+++ main.cpp
@@ -17,23 +17,17 @@ private :
     void getPrepared2DArray();
     void setArray(float ** ar, int r, int c);
 
-
     float* arrayWithCoefficients(int step);
     void defaultElimination();
-    float* getResultFromRowEchelonForm();  // macierz schodkowa
-    bool isDiagonalValueEqualZero(int i, int j);
-    bool isFirstElementEqualZero();
+    void getResultFromRowEchelonForm();  // macierz schodkowa
+    bool isElementEqualZero(int i, int j);
+    bool isEquationSolvable();
 public :
 
     ~Gauss(){delete2Darray(this->array, this->rows);}
     void menu();
 
 };
-
-
-
-
-
 
 
 
@@ -76,7 +70,8 @@ void Gauss::getPrepared2DArray(){
     float example1[4][5] = { {1,1,0,-3,1}, {1,4,-1,-4,-2},{0.5,0.5,-3,-5.5,1.5} ,{1.5,3,-5,-9,-0.5}};
     float example2[4][5] = {{0,2,-1,2,0} ,{0,0,-2,4,4}, {0,-3,1.5,7,0}, {1,-1,1,6,-1}};
     float example3[4][5] = { {2.25,-2.5,4,-5.25,-1}, {-3,-7.5,6.5,0,17},{-6.25,-12.5,0.25,5.25,24.25},{9,10,7,-21,-33}};
-    float example4[3][4] = { {1,2,-1,-1},{-1,-1,3,4},{-2,-3,1,-1}};
+    //float example4[3][4] = { {1,2,-1,-1},{-1,0,3,4},{-2,-3,1,-1}};
+    float example4[3][4] = { {0,2,-1,0},{1,0,-2,4},{10,0,3,0}};
 
     int chooice;
     cout << "\nPrzyklad 1 : wynik poprawny  x4=-1 x3=1 x2=-1 x1=-1";
@@ -107,6 +102,10 @@ void Gauss::getPrepared2DArray(){
         float **ptr = copy2DArrayToPointer(example4,3,4);
         this->setArray(ptr, 3,4);
     }
+    else{
+        cout << " wybrano zly numer przykladu";
+        exit(0);
+    }
 }
 
 
@@ -119,8 +118,11 @@ void Gauss::pickMethod(){
     cin >> choice;
     switch(choice){
         case 1:{
-            this->defaultElimination();
-            this->getResultFromRowEchelonForm();
+            defaultElimination();
+            if(isEquationSolvable()){
+                 getResultFromRowEchelonForm();
+            }
+
             break;
         }
         case 2:{
@@ -133,7 +135,7 @@ void Gauss::pickMethod(){
         }
     }
 }
-float* Gauss::getResultFromRowEchelonForm(){
+void Gauss::getResultFromRowEchelonForm(){
 
     int rows = this->rows;
     float **ar = this->array;
@@ -184,9 +186,9 @@ void Gauss::defaultElimination(){
     int rows = this->rows;
     int cols = this->cols;
     int temp = 1;
-    while(isFirstElementEqualZero()){
+    while(isElementEqualZero(0,0)){
         if(temp == rows){
-            cout << "ukladu nie mozna rozwiazac tym sposobem";
+            cout << "ukladu ma nieskonczenie wiele rozwiazan";
             exit(15);
         }
         swapRows(this->array, cols, 0, temp);
@@ -196,18 +198,31 @@ void Gauss::defaultElimination(){
 
     float *coefficients  ;
     while(step < rows){
-        coefficients = arrayWithCoefficients(step);
-        cout << "\n step " << step << endl;
-        if(isDiagonalValueEqualZero(i,j)){
-                    //przesuniecie wierszy ?
-            cout << " \n\n trzeba przesunac wiersze";
+        if(isElementEqualZero(step,step)){
+            cout << "\n elemnt na przekatnej = 0, zamiana wierszy  ";
+            int temp = step + 1;
+            while(temp < rows || isElementEqualZero(step,step)){
+                swapRows(this->array,cols, step, temp );
+                print2DArray(this->array,rows,cols);
+                temp++;
             }
+            if(isElementEqualZero(step,step)){
+                cout << "element na przekatnej = 0 w kazdym ukladzie wierszy, dana metoda gaussa nie moze byc kontynuowana";
+                exit(3);
+            }
+            }
+        cout << "\n\n krok " << step << endl;
+        coefficients = arrayWithCoefficients(step);
         for(int i = step ; i < rows ; i++){
             for(int j = step-1; j < cols ;j++){
-                this->array[i][j] -=   (coefficients[i]*this->array[step-1][j]);
+                if(coefficients[i]){
+                    this->array[i][j] -=   (coefficients[i]*this->array[step-1][j]);
+                }
+                else  {
+                        cout << " wspolczynnik dla wiersza nr " << i + 1 << " = 0, wartosci w tym wierszu nie zmienia sie \n";
+                        j = cols -1;
+                }
             }
-
-
         }
         print2DArray(this->array,rows,cols);
         step++;
@@ -215,14 +230,28 @@ void Gauss::defaultElimination(){
     }
 
 }
-bool Gauss::isFirstElementEqualZero(){
-    return (this->array[0][0] <= this->numberCloseToZero);
-}
-bool Gauss::isDiagonalValueEqualZero(int i, int j){
-    return i == j && fabs(this->array[i][j]) <= this->numberCloseToZero;
-}
+
 void Gauss::setArray(float ** ar, int r, int c){
         this->array = ar;
         this->rows = r;
         this->cols = c;
     }
+
+bool Gauss::isEquationSolvable(){
+    int lastColumn = this->cols - 1;
+    int lastRow = this->rows - 1;
+    if(this->array[lastRow][lastColumn - 1] == 0 && this->array[lastRow][lastColumn] == 0 ){
+        cout << "Uklad rownan ma nieskonczenie wiele rozwiazan ";
+        return false;
+    }
+    else if(this->array[lastRow][lastColumn - 1] == 0 && this->array[lastRow][lastColumn] != 0){
+        cout << "Uklad sprzeczny - nie ma zadnego rozwiazania ";
+        return false;
+    }
+    return true;
+
+}
+
+bool Gauss::isElementEqualZero(int i, int j){
+    return  fabs(this->array[i][j]) <= this->numberCloseToZero;
+}
